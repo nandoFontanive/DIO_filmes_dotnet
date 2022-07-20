ff7eda1bcc5793beb31d7cc1f9792ba19482d8d8
nandoFontanive <nando.fontanive@gmail.com>
2022-07-20 11:59:09 -0300

    Primeiro envio

---
+++ Enum/Genero.cs
@@ -0,0 +1,23 @@
+namespace DIO.Series
+{
+	public enum Genero
+	{
+		Acao = 1,
+		Aventura = 2,
+		Comedia = 3,
+		Documentario = 4,
+		Drama = 5,
+		Espionagem = 6,
+		Faroeste = 7,
+		Fantasia = 8,
+		Ficcao_cientifica_passado = 9,
+		Ficcao_cientifica_futuro = 10,
+		Historico = 11,
+		Noir = 12,
+		Romance = 13,
+		Suspense = 14,
+		Thriller = 15,
+		Terror = 16,
+		Musical = 17
+	}
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Projeto do Bootcamp DIO - Unimed-BH - Prática .NET
+
+Programa de cadastro de filmes em memória em .NET
